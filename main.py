7bb0bebfaef3cfe3df382129324baf4b45819451
9hidekikuriyama-coder <9.hideki.kuriyama@gmail.com>
2026-02-09 21:50:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
 import yfinance as yf
-import pandas as pd
 
 app = FastAPI()
 
 @app.post("/stocks")
 def stocks(payload: dict):
-    codes = payload["codes"]
     result = {}
 
-    for code in codes:
+    for code in payload["codes"]:
         ticker = f"{code}.T"
-        df = yf.download(ticker, period="5d", progress=False)
-        df = df.dropna()
+
+        df = yf.download(
+            ticker,
+            period="5d",
+            progress=False,
+            threads=False,
+            auto_adjust=False
+        ).dropna()
+
         if len(df) < 2:
             continue
 
@@ -20,12 +25,10 @@ def stocks(payload: dict):
         prev  = df.iloc[-2]
 
         result[code] = {
-            "today_close":  float(today["Close"]),
-            "today_volume": int(today["Volume"]),
-            "prev_close":   float(prev["Close"]),
-            "prev_volume":  int(prev["Volume"])
+            "today_close":  float(today["Close"].iloc[0]),
+            "today_volume": int(today["Volume"].iloc[0]),
+            "prev_close":   float(prev["Close"].iloc[0]),
+            "prev_volume":  int(prev["Volume"].iloc[0])
         }
 
-    return {
-        "data": result
-    }
+    return {"data": result}
